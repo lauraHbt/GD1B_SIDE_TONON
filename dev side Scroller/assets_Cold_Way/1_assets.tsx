9ab6f4c217d82d391b38102c4315e86efa9ca8c9
HERBERT Laura <93705427+lauraHbt@users.noreply.github.com>
2022-02-06 16:18:50 +0100

    Add files via upload

---
+++ dev side Scroller/assets_Cold_Way/1_assets.tsx
@@ -0,0 +1,146 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="1_assets" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="1_assets.png" width="640" height="640"/>
+ <tile id="2">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="14" x="32.3333" y="-0.333333">
+    <polygon points="0,0 -32,31 -0.666667,31.6667"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="15" y="32.3333">
+    <polygon points="0,0 -15.3333,0 -15.3333,-32.6667 11.3333,-23.3333 -15,-19 0,-13 -13,-10.6667"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="31.3333" y="-0.333333">
+    <polygon points="0,0 -27,9.33333 -3.33333,13.3333 -16,20.3333 -0.333333,23.6667 -14.3333,31.6667 0,31.3333"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0.333333" y="0.333333" width="31.3333" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="5" x="0.333333" y="0.666667" width="32" height="30.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="31.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.666667" width="32" height="30.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0.333333" y="30.3333">
+    <polygon points="0,0 9.33333,-26 11.6667,1 21,-14.3333 23,1 33.3333,-15 31.6667,0.666667"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0.666667" y="17">
+    <polygon points="0,0 2.33333,-9.66667 8.66667,15.3333 12.6667,-1.33333 17,14.3333 22.6667,-16 33,12.3333 31,14.3333 -1,15"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="31.6667">
+    <polygon points="0,0 31.3333,-8 1.33333,-14.6667 16.3333,-19.6667 1,-24 22,-28.3333 -2,-33.6667"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="31" y="31.6667">
+    <polygon points="0,0 -9,-0.666667 -29.6667,-7.66667 -0.666667,-14.6667 -16.3333,-18.6667 -1.66667,-24.3333 -23.3333,-27.3333 -8.66667,-31.6667 0,-31.6667"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0.333333" y="0" width="31" height="31.3333"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="31.6667" height="31.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.333333" width="31.6667" height="31.3333"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.666667" width="31.6667" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0.333333" y="0.333333" width="31.6667" height="31.3333"/>
+  </objectgroup>
+ </tile>
+</tileset>
